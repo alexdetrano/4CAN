3774a9c309a79991f691d1901574acecc68d7173
alexdetrano <alexdetrano@gmail.com>
2020-08-06 09:44:05 -0400

    added better arg parsing and broke out parsing into its own class

--- tools/transcan.py
+++ tools/transcan.py
@@ -10,6 +10,8 @@ __author__ = "adetrano"
 
 suffix = b'DBEEF'
 
+
+
 class Listener(can.Listener):
     def __init__(self):
         self.hits = []
@@ -32,42 +34,63 @@ class Listener(can.Listener):
         for sent_id, recv_id in self.msg_map:
             print('sent ID: {} recv ID: {:03x}'.format(sent_id, recv_id))
 
+class Bus2Bus:
+    def __init__(self, src_bus, dst_bus):
+        self.src_bus = can.interface.Bus(src_bus, bustype='socketcan')
+        self.dst_bus = can.interface.Bus(dst_bus, bustype='socketcan')
+        self.listener = [Listener()][0]
+        #self.src_ch = src_bus.channel
+        #self.dst_ch = dst_bus.channel
 
+    def send_msgs(self, start_id=0, end_id=0x7ff):
+        for id in range(start_id, end_id+1):
+            # include the sending ID as part of the payload
+            # so we know original sent ID
+            prefix = (id).to_bytes(3, byteorder='big')
+            payload = prefix + suffix
+
+            # extended_id determines if ID is 29 bits or 7 bits
+            msg = can.Message(arbitration_id=id, data=payload, extended_id=False)
+
+            # send message
+            print(f'Sending 0x{id:03x}', end='\r')
+            self.src_bus.send(msg)
+            time.sleep(0.05)
+
+    def process_msgs(self):
+        self.listener.process_messages()
+
+    def print_msgs(self):
+        self.listener.print_messages()
+
+    def cleanup(self):
+        self.listener.stop()
+        self.src_bus.shutdown()
+        self.dst_bus.shutdown()
 
 def usage():
     script_name = os.path.basename(sys.argv[0])
     print('Usage: {} send_if recv_if'.format(script_name))
     print('eg {} can0 can1'.format(script_name))
+    print('requires python-can - install with `pip3 install python-can`')
 
-def main(can_if_send, can_if_recv):
-    bus_send = can.interface.Bus(can_if_send, bustype='socketcan')
-    bus_recv = can.interface.Bus(can_if_recv, bustype='socketcan')
-    listener = [Listener()]  # needs to be an iterable
-    #notifier = can.Notifier(bus_recv, listener)
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Test interbus CAN bus connectivity')
+    parser.add_argument('src', metavar='src_interface', help='interface to send CAN messages from')
+    parser.add_argument('dst', metavar='dst_interface', help='interface to receive CAN messages on')
+    #parser.add_argument('--permute', nargs='+', metavar='interface', help='send/receive on all possible interface permutations')
 
-    max_can_id =  0x7ff
+    return parser.parse_args(args)
 
-    for id in range(0, max_can_id):
-        # include the sending ID as part of the payload
-        # so we know original sent ID
-        #prefix = (int.from_bytes(b'000', byteorder='big') | id).to_bytes(3, byteorder='big')
-        prefix = (id).to_bytes(3, byteorder='big')
-        payload = prefix + suffix
+def main():
+    args = parse_args(sys.argv[1:])
+    print(args)
+    bus2bus = Bus2Bus(args.src, args.dst)
+    bus2bus.send_msgs()
+    bus2bus.cleanup()
 
-        # extended_id determines if ID is 29 bits or 7 bits
-        msg = can.Message(arbitration_id=id, data=payload, extended_id=False)
 
-        # send message
-        bus_send.send(msg)
-        time.sleep(0.1)
-
-    listener[0].process_messages()
-    listener[0].print_messages()
 
 
 if __name__ == '__main__':
-    # TOdo: add argparse and start/stop IDs
-    if len(sys.argv) < 3:
-        usage()
-    else:
-        main(sys.argv[1], sys.argv[2])
+    main()
